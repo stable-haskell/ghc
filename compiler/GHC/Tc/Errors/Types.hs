zepalmer/intensional-functions-ghc

compiler: Default and warn ExceptionContext constraints

compiler/GHC/Tc/Errors/Types.hs
@@ -3178,6 +3178,14 @@ data TcRnMessage where
   -}
   TcRnUnknownTyVarsOnRhsOfInjCond :: [Name] -> TcRnMessage
 
+  {-| TcRnDefaultedExceptionContext is a warning that is triggered when the
+      backward-compatibility logic solving for implicit ExceptionContext
+      constraints fires.
+
+      Test cases: TODO
+  -}
+  TcRnDefaultedExceptionContext :: CtLoc -> TcRnMessage
+
   deriving Generic
 
 -- | Things forbidden in @type data@ declarations.

stable-haskell/ghc

compiler/GHC/Tc/Errors/Types.hs
@@ -4263,6 +4263,14 @@ data TcRnMessage where
   -}
   TcRnNamespacedFixitySigWithoutFlag :: FixitySig GhcPs -> TcRnMessage
 
+  {-| TcRnDefaultedExceptionContext is a warning that is triggered when the
+      backward-compatibility logic solving for implicit ExceptionContext
+      constraints fires.
+
+      Test cases: DefaultExceptionContext
+  -}
+  TcRnDefaultedExceptionContext :: CtLoc -> TcRnMessage
+
   deriving Generic
 
 ----