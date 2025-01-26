cc7048a0bb806dac3f2f8a9c66a8842fa233043a
Andrea Bedini <andrea@andreabedini.com>
2025-01-27 10:49:19 +1100

    Hack LLVM version bound

--- compiler/GHC/CmmToLlvm/Version.hs
+++ compiler/GHC/CmmToLlvm/Version.hs
@@ -12,29 +12,25 @@ where
 import GHC.Prelude
 
 import GHC.CmmToLlvm.Version.Type
-import GHC.CmmToLlvm.Version.Bounds
 
-import Data.Char (isDigit)
 import Data.List (intercalate)
 import qualified Data.List.NonEmpty as NE
 
-parseLlvmVersion :: String -> Maybe LlvmVersion
-parseLlvmVersion =
-    fmap LlvmVersion . NE.nonEmpty . go [] . dropWhile (not . isDigit)
-  where
-    go vs s
-      | null ver_str
-      = reverse vs
-      | '.' : rest' <- rest
-      = go (read ver_str : vs) rest'
-      | otherwise
-      = reverse (read ver_str : vs)
-      where
-        (ver_str, rest) = span isDigit s
+-- HACK HACK
+supportedLlvmVersionLowerBound :: LlvmVersion
+supportedLlvmVersionLowerBound = LlvmVersion (NE.singleton 0)
 
+-- HACK HACK
+supportedLlvmVersionUpperBound :: LlvmVersion
+supportedLlvmVersionUpperBound = LlvmVersion (NE.singleton 1000)
+
+-- HACK HACK
+parseLlvmVersion :: String -> Maybe LlvmVersion
+parseLlvmVersion _ = Nothing
+
+-- HACK HACK
 llvmVersionSupported :: LlvmVersion -> Bool
-llvmVersionSupported v =
-  v >= supportedLlvmVersionLowerBound && v < supportedLlvmVersionUpperBound
+llvmVersionSupported _ = True
 
 llvmVersionStr :: LlvmVersion -> String
 llvmVersionStr = intercalate "." . map show . llvmVersionList
