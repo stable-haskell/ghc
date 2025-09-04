stable-haskell/ghc

commit 7442ca3f8b3d737a714dec76df9573beed8059de

Incorporate Julians changes and re-run autoreconf.
This incporates the changes from:

git diff ff1f2c08ac62516053cbc68362d51330a336bbfb...cf217cd777fe18b3234029218b5309186352500a

rts/configure.ac
@@ -178,9 +178,11 @@ dnl Check for libraries
 dnl ################################################################
 
 dnl ** check whether we need -ldl to get dlopen()
-AC_CHECK_LIB([dl], [dlopen])
+AC_CHECK_LIB([dl], [dlopen], [HAVE_LIBDL=1])
 dnl ** check whether we have dlinfo
 AC_CHECK_FUNCS([dlinfo])
+
+FP_CHECK_PTHREAD_LIB
 
 dnl --------------------------------------------------
 dnl * Miscellaneous feature tests
@@ -520,6 +522,16 @@ cat $srcdir/rts.buildinfo.in \
     || exit 1
 rm -f external-symbols.flags
 ]
+
+AS_IF(
+  [test "$HAVE_LIBDL" = 1],
+  [EXTRA_LIBS="$EXTRA_LIBS dl"])
+AS_IF(
+  [test "$UseLibpthread" = "YES"]
+  [EXTRA_LIBS="$EXTRA_LIBS pthread"])
+AS_IF(
+  [test "x$EXTRA_LIBS" != "x"]
+  [echo "extra-libraries: $EXTRA_LIBS" >> rts.buildinfo])
 
 dnl --------------------------------------------------------------
 dnl Generate derived constants

configure.ac
@@ -2,7 +2,7 @@
 AC_PREREQ([2.69])
 AC_INIT([ghc-builder], [0.1.0], [your-email@example.com])
 AC_CONFIG_SRCDIR([.]) # A representative .in file
-AC_CONFIG_AUX_DIR([build-aux]) # Recommended place for config.guess, config.sub
+#AC_CONFIG_AUX_DIR([build-aux]) # Recommended place for config.guess, config.sub
 AC_CONFIG_MACRO_DIR([m4])      # For any custom m4 macros
 # AM_INIT_AUTOMAKE([-Wall -Werror foreign]) # Using some automake conventions
 

Makefile
@@ -79,7 +79,7 @@
 #       submodules.
 
 SHELL := bash
-.SHELLFLAGS := -eu -o pipefail -O globstar -c
+.SHELLFLAGS := -eu -o pipefail -c
 
 VERBOSE ?= 0
 
@@ -95,6 +95,8 @@ EMCC ?= emcc
 EMCXX ?= em++
 EMAR ?= emar
 EMRANLIB ?= emranlib
+
+GHC_CONFIGURE_ARGS ?=
 
 EXTRA_LIB_DIRS ?=
 EXTRA_INCLUDE_DIRS ?=
@@ -143,6 +145,7 @@ define GHC_INFO
 $(shell sh -c "$(GHC) --info | $(GHC0) -e 'getContents >>= foldMap putStrLn . lookup \"$1\" . read'")
 endef
 
+HOST_PLATFORM   = $(call GHC_INFO,Host platform)
 TARGET_PLATFORM = $(call GHC_INFO,target platform string)
 TARGET_ARCH     = $(call GHC_INFO,target arch)
 TARGET_OS       = $(call GHC_INFO,target os)
@@ -632,8 +635,9 @@ stage2: $(addprefix _build/stage2/bin/,$(STAGE2_EXECUTABLES)) _build/stage2/lib/
 
 # --- Stage 3 generic ---
 
-_build/stage3/lib/targets/%:
-	@mkdir -p $@
+_build/stage2/lib/targets/%:
+	@mkdir -p _build/stage3/lib/targets/$(@F)
+	@rm -f _build/stage2/lib/targets/$(@F)
 	@mkdir -p _build/stage2/lib/targets/
 	@ln -sf ../../../stage3/lib/targets/$(@F) _build/stage2/lib/targets/$(@F)
 
@@ -672,7 +676,7 @@ endef
 .PHONY: stage3-javascript-unknown-ghcjs
 stage3-javascript-unknown-ghcjs: _build/stage3/lib/targets/javascript-unknown-ghcjs/lib/settings javascript-unknown-ghcjs-libs _build/stage3/lib/targets/javascript-unknown-ghcjs/lib/package.conf.d/package.cache
 
-_build/stage3/lib/targets/javascript-unknown-ghcjs/lib/settings: _build/stage3/lib/targets/javascript-unknown-ghcjs _build/stage1/bin/ghc-toolchain-bin
+_build/stage3/lib/targets/javascript-unknown-ghcjs/lib/settings: _build/stage2/lib/targets/javascript-unknown-ghcjs _build/stage1/bin/ghc-toolchain-bin
 	@mkdir -p $(@D)
 	_build/stage1/bin/ghc-toolchain-bin $(GHC_TOOLCHAIN_ARGS) --triple javascript-unknown-ghcjs --output-settings -o $@ --cc $(EMCC) --cxx $(EMCXX) --ar $(EMAR) --ranlib $(EMRANLIB)
 
@@ -697,7 +701,7 @@ javascript-unknown-ghcjs-libs: _build/stage3/bin/javascript-unknown-ghcjs-ghc-pk
 .PHONY: stage3-x86_64-musl-linux
 stage3-x86_64-musl-linux: x86_64-musl-linux-libs _build/stage3/lib/targets/x86_64-musl-linux/lib/package.conf.d/package.cache
 
-_build/stage3/lib/targets/x86_64-musl-linux/lib/settings: _build/stage3/lib/targets/x86_64-musl-linux _build/stage1/bin/ghc-toolchain-bin
+_build/stage3/lib/targets/x86_64-musl-linux/lib/settings: _build/stage2/lib/targets/x86_64-musl-linux _build/stage1/bin/ghc-toolchain-bin
 	@mkdir -p $(@D)
 	_build/stage1/bin/ghc-toolchain-bin $(GHC_TOOLCHAIN_ARGS) --triple x86_64-musl-linux --output-settings -o $@ --cc x86_64-unknown-linux-musl-cc --cxx x86_64-unknown-linux-musl-c++ --ar x86_64-unknown-linux-musl-ar --ranlib x86_64-unknown-linux-musl-ranlib --ld x86_64-unknown-linux-musl-ld
 
@@ -722,7 +726,7 @@ x86_64-musl-linux-libs: _build/stage3/bin/x86_64-musl-linux-ghc-pkg _build/stage
 .PHONY: stage3-wasm32-unknown-wasi
 stage3-wasm32-unknown-wasi: wasm32-unknown-wasi-libs _build/stage3/lib/targets/wasm32-unknown-wasi/lib/package.conf.d/package.cache _build/stage3/lib/targets/wasm32-unknown-wasi/lib/dyld.mjs _build/stage3/lib/targets/wasm32-unknown-wasi/lib/post-link.mjs _build/stage3/lib/targets/wasm32-unknown-wasi/lib/prelude.mjs
 
-_build/stage3/lib/targets/wasm32-unknown-wasi/lib/settings: _build/stage3/lib/targets/wasm32-unknown-wasi _build/stage1/bin/ghc-toolchain-bin
+_build/stage3/lib/targets/wasm32-unknown-wasi/lib/settings: _build/stage2/lib/targets/wasm32-unknown-wasi _build/stage1/bin/ghc-toolchain-bin
 	@mkdir -p $(@D)
 	PATH=/home/hasufell/.ghc-wasm/wasi-sdk/bin:$(PATH) _build/stage1/bin/ghc-toolchain-bin $(GHC_TOOLCHAIN_ARGS) --triple wasm32-unknown-wasi --output-settings -o $@ --cc wasm32-wasi-clang --cxx wasm32-wasi-clang++ --ar ar --ranlib ranlib --ld wasm-ld --merge-objs wasm-ld --merge-objs-opt="-r" --disable-ld-override --disable-tables-next-to-code $(foreach opt,$(WASM_CC_OPTS),--cc-opt=$(opt)) $(foreach opt,$(WASM_CXX_OPTS),--cxx-opt=$(opt))
 
@@ -731,11 +735,6 @@ _build/stage3/lib/targets/wasm32-unknown-wasi/lib/package.conf.d/package.cache: 
 	@rm -rf $(@D)/*
 	cp -rfp _build/stage3/wasm32-unknown-wasi/packagedb/host/*/* $(@D)
 	_build/stage3/bin/wasm32-unknown-wasi-ghc-pkg recache
-
-# ghc-toolchain borks unlit
-_build/stage3/lib/targets/wasm32-unknown-wasi/bin/unlit: _build/stage2/bin/unlit
-	@mkdir -p $(@D)
-	cp -rfp $< $@
 
 .PHONY: wasm32-unknown-wasi-libs
 wasm32-unknown-wasi-libs: private GHC=$(abspath _build/stage3/bin/wasm32-unknown-wasi-ghc)
@@ -815,14 +814,14 @@ _build/bindist: stage2 driver/ghc-usage.txt driver/ghci-usage.txt
 	@cp -rfp _build/stage2/bin/* $@/bin/
 	# Copy libraries and settings from stage2 lib
 	@cp -rfp _build/stage2/lib/{package.conf.d,settings,template-hsc.h} $@/lib/
-	@mkdir -p $@/lib/x86_64-linux
+	@mkdir -p $@/lib/$(HOST_PLATFORM)
 	@cd $@/lib/package.conf.d ; \
 		for pkg in *.conf ; do \
 		  pkgname=`echo $${pkg} | sed 's/-[0-9.]*\(-[0-9a-zA-Z]*\)\?\.conf//'` ; \
 		  pkgnamever=`echo $${pkg} | sed 's/\.conf//'` ; \
-		  mkdir -p $(CURDIR)/$@/lib/x86_64-linux/$${pkg%.conf} ; \
-		  cp -rfp $(CURDIR)/_build/stage2/build/host/x86_64-linux/ghc-*/$${pkg%.conf}/build/* $(CURDIR)/$@/lib/x86_64-linux/$${pkg%.conf} ; \
-		  $(call patchpackageconf,$${pkgname},$${pkg},../../..,x86_64-linux,$${pkgnamever}) ; \
+		  mkdir -p $(CURDIR)/$@/lib/$(HOST_PLATFORM)/$${pkg%.conf} ; \
+		  cp -rfp $(CURDIR)/_build/stage2/build/host/*/ghc-*/$${pkg%.conf}/build/* $(CURDIR)/$@/lib/$(HOST_PLATFORM)/$${pkg%.conf} ; \
+		  $(call patchpackageconf,$${pkgname},$${pkg},../../..,$(HOST_PLATFORM),$${pkgnamever}) ; \
 		done
 	# Copy driver usage files
 	@cp -rfp driver/ghc-usage.txt $@/lib/
@@ -844,7 +843,7 @@ _build/bindist/ghc.tar.gz: _build/bindist
 		lib/package.conf.d \
 		lib/settings \
 		lib/template-hsc.h \
-		lib/x86_64-linux
+		lib/$(HOST_PLATFORM)
 
 _build/bindist/lib/targets/%: _build/bindist driver/ghc-usage.txt driver/ghci-usage.txt stage3-%
 	@echo "::group::Creating binary distribution in $@"
@@ -880,15 +879,15 @@ $(GHC1) $(GHC2): | hackage
 hackage: _build/packages/hackage.haskell.org/01-index.tar.gz
 _build/packages/hackage.haskell.org/01-index.tar.gz: | $(CABAL)
 	@mkdir -p $(@D)
-	$(CABAL) $(CABAL_ARGS) update --index-state 2025-04-22T01:25:40Z
+	$(CABAL) $(CABAL_ARGS) update --index-state @1745256340
 
 # booted depends on successful source preparation
 _build/booted: libraries/ghc-boot-th-next/.synth-stamp
 	@echo "::group::Running ./boot script..."
 	@mkdir -p _build/logs
-	./boot |& tee _build/logs/boot.log
+	./boot
 	@echo ">>> Running ./configure script..."
-	./configure
+	./configure $(GHC_CONFIGURE_ARGS)
 	touch $@
 	@echo "::endgroup::"
 

compiler/ghc.cabal.in
@@ -34,7 +34,7 @@ extra-source-files:
     CodeGen.Platform.h
 
 custom-setup
-    setup-depends: base >= 3 && < 5, Cabal >= 1.6 && <3.16, directory, process, filepath, containers
+    setup-depends: base >= 3 && < 5, Cabal >= 1.6 && <3.18, directory, process, filepath, containers
 
 Flag internal-interpreter
     Description: Build with internal interpreter support.

libraries/ghc-boot/ghc-boot.cabal.in
@@ -28,7 +28,7 @@ build-type:     Custom
 extra-source-files: changelog.md
 
 custom-setup
-    setup-depends: base >= 3 && < 5, Cabal >= 1.6 && <3.16, directory, filepath
+    setup-depends: base >= 3 && < 5, Cabal >= 1.6 && <3.18, directory, filepath
 
 source-repository head
     type:     git

rts/rts.cabal
@@ -259,9 +259,6 @@ flag libm
 flag librt
   default: False
   manual: True
-flag libdl
-  default: False
-  manual: True
 flag libffi-adjustors
   default: False
   manual: True
@@ -555,8 +552,6 @@ common rts-link-options
        extra-libraries: m
     if flag(librt)
        extra-libraries: rt
-    if flag(libdl)
-       extra-libraries: dl
     if os(windows)
        extra-libraries:
           wsock32 gdi32 winmm

commit 471aa2f0a42361077e8f4e9e7098af720a922190

rts/configure: derive extra-libraries from LIBS (dl, pthread)
Cherry-pick only m4/fp_check_pthreads.m4 and rts/configure.ac changes from 09400675192e394fc70badfd1eedc38bfe0ad3b9 onto stable-ghc-9.14.

Fixes CI failure: Missing (or bad) C libraries: dl, pthread introduced by 7442ca3f8b3d737a714dec76df9573beed8059de. We now rely on AC_SEARCH_LIBS for dlopen() and pthread_create and derive extra-libraries from LIBS instead of manual bookkeeping via HAVE_LIBDL/UseLibpthread variables.

This ensures platforms requiring explicit -ldl/-lpthread are covered while not adding them unnecessarily where they aren't needed.

Cherry-picked-from: 09400675192e394fc70badfd1eedc38bfe0ad3b9

m4/fp_check_pthreads.m4
@@ -12,22 +12,7 @@ AC_DEFUN([FP_CHECK_PTHREAD_LIB],
   dnl
   dnl Note that it is important that this happens before we AC_CHECK_LIB(thread)
   AC_MSG_CHECKING(whether -lpthread is needed for pthreads)
-  AC_CHECK_FUNC(pthread_create,
-      [
-          AC_MSG_RESULT(no)
-          UseLibpthread=NO
-      ],
-      [
-          AC_CHECK_LIB(pthread, pthread_create,
-              [
-                  AC_MSG_RESULT(yes)
-                  UseLibpthread=YES
-              ],
-              [
-                  AC_MSG_RESULT([no pthreads support found.])
-                  UseLibpthread=NO
-              ])
-      ])
+  AC_SEARCH_LIBS([pthread_create],[pthread])
 ])
 
 # FP_CHECK_PTHREAD_FUNCS
@@ -37,6 +22,7 @@ AC_DEFUN([FP_CHECK_PTHREAD_LIB],
 # `AC_DEFINE`s various C `HAVE_*` macros.
 AC_DEFUN([FP_CHECK_PTHREAD_FUNCS],
 [
+  OLD_LIBS=$LIBS
   dnl Setting thread names
   dnl ~~~~~~~~~~~~~~~~~~~~
   dnl The portability situation here is complicated:
@@ -123,4 +109,5 @@ AC_DEFUN([FP_CHECK_PTHREAD_FUNCS],
   )
 
   AC_CHECK_FUNCS_ONCE([pthread_condattr_setclock])
+  LIBS=$OLD_LIBS
 ])

rts/configure.ac
@@ -178,7 +178,7 @@ dnl Check for libraries
 dnl ################################################################
 
 dnl ** check whether we need -ldl to get dlopen()
-AC_CHECK_LIB([dl], [dlopen], [HAVE_LIBDL=1])
+AC_SEARCH_LIBS([dlopen], [dl])
 dnl ** check whether we have dlinfo
 AC_CHECK_FUNCS([dlinfo])
 
@@ -523,14 +523,9 @@ cat $srcdir/rts.buildinfo.in \
 rm -f external-symbols.flags
 ]
 
+AC_SUBST([EXTRA_LIBS],[` printf " %s " "$LIBS" | sed -E 's/ -l([[^ ]]*)/\1 /g' `])
 AS_IF(
-  [test "$HAVE_LIBDL" = 1],
-  [EXTRA_LIBS="$EXTRA_LIBS dl"])
-AS_IF(
-  [test "$UseLibpthread" = "YES"]
-  [EXTRA_LIBS="$EXTRA_LIBS pthread"])
-AS_IF(
-  [test "x$EXTRA_LIBS" != "x"]
+  [test "`echo $EXTRA_LIBS | sed 's/ //'`" != ""],
   [echo "extra-libraries: $EXTRA_LIBS" >> rts.buildinfo])
 
 dnl --------------------------------------------------------------